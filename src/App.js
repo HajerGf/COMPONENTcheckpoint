a3633dca89ed6bcec77fbb10eea7b13bacda2ad5
HajerGf <119872821+HajerGf@users.noreply.github.com>
2023-02-10 10:31:13 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Address from './Components/Profile/Address';
 import FullName from './Components/Profile/FullName';
 import ProfilePhoto from './Components/Profile/ProfilePhoto';
 
-
-
 function App() {
 
 
--- src/Components/Profile/ProfilePhoto.js
+++ src/Components/Profile/ProfilePhoto.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfilePhoto = () => {
     return (
         <div>
-            <img src="https://pbs.twimg.com/profile_images/1445296982814339074/lypCHbZA_400x400.jpg" alt="image" />
+            <img src="https://pbs.twimg.com/profile_images/1445296982814339074/lypCHbZA_400x400.jpg" alt="df" />
         </div>
     )
 }
